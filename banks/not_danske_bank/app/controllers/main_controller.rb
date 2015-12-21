51f4ce44564162a2d393b02c8ce28774911d7d0c
Adam Vongrej <adamv@Adams-MacBook-Air.local>
2015-12-21 11:18:01 +0100

    Changes & Fixes

--- banks/not_danske_bank/app/controllers/main_controller.rb
+++ banks/not_danske_bank/app/controllers/main_controller.rb
@@ -11,20 +11,30 @@ class MainController < ApplicationController
     puts params.inspect
 
     # Persist to the database for some reason
-    request = LoanRequest.new
-    request.ssn = params[:ssn]
-    request.creditScore = params[:creditScore]
-    request.loanAmount = params[:loanAmount]
-    request.loanDuration = params[:loanDuration]
-    request.save
+    req = LoanRequest.new
+    req.ssn = params[:ssn]
+    req.creditScore = params[:creditScore]
+    req.loanAmount = params[:loanAmount]
+    req.loanDuration = params[:loanDuration]
+    req.save
+
+    reply_to = request.headers['HTTP_REPLY_TO']
 
     #TODO: Get a job in a bank and find out how this are calculated
     randomInterestRate = randomInterest(1, 5).round(2)
 
     result = {
       interestRate: randomInterestRate,
-      ssn: request.ssn
+      ssn: req.ssn
     }
+
+    conn = Bunny.new
+    conn.start
+    ch = conn.create_channel
+    q = ch.queue(reply_to)
+
+    ch.default_exchange.publish(result.to_json, routing_key: q.name)
+
     render :json => result.to_json
   end
 
--- translator/translator_not_danske_bank/translator.php
+++ translator/translator_not_danske_bank/translator.php
@@ -9,15 +9,32 @@ require_once('abstract_translator.php');
 class Translator extends AbstractTranslator {
 
     protected function translate_request($request){
+        //Request is all right as it is
         $request = json_decode($request, true);
+        return $request;
+    }
+
+    protected function initialize_bank(){
+        //Nothing needed here, its asimple curl request}
+    }
+
+    protected function query_bank($req_msg){
+        //Just to maintain the same control flow
+        $request = $this->translate_request($req_msg->body);
         var_dump($request);
-        $formatted_request = array();
-        $formatted_request['ssn'] = implode('', explode('-', $request['ssn']));
-        $formatted_request['creditScore'] = $request['credit_score'];
-        $formatted_request['loanAmount'] = $request['amount'];
-            $formatted_request['loanDuration'] = $request['duration'];
-        var_dump($formatted_request);
-        return json_encode($formatted_request);
+
+        // Get cURL resource
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_HTTPHEADER => array('reply_to' => $this->bank_params['reply_to']),
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $request,
+            CURLOPT_URL => $this->bank_params['host']
+        ));
+
+        curl_exec($curl);
+        curl_close($curl);
     }
 }
 
@@ -30,12 +47,8 @@ $translator_params = array(
 );
 
 $bank_params = array(
-    'host' => 'localhost',
-    'port' => '5672',
-    'username' => 'test',
-    'password' => 'test',
-    'exchange' => 'bank_not_nordea',
-    'reply_to' => 'vongrad_not_nordea'
+    'host' => 'http://localhost/createLoanRequest',
+    'reply_to' => 'vongrad_not_danske_bank'
 );
 
 $translator = new Translator($translator_params, $bank_params);
